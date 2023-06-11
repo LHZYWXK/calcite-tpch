ebc7feb1e32f3ba59817ecbeecfd75457ba9f7b3
Yuqilin0102 <1210887039@qq.com>
2023-06-11 16:56:30 +0800

    modify plan interface, call it by react ajax

--- src/main/java/hk/hku/cs/calcite/CalciteApplication.java
+++ src/main/java/hk/hku/cs/calcite/CalciteApplication.java
@@ -3,7 +3,9 @@ package hk.hku.cs.calcite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@EnableSwagger2
 public class CalciteApplication {
 
     public static void main(String[] args) {
--- src/main/java/hk/hku/cs/calcite/controller/PlanController.java
+++ src/main/java/hk/hku/cs/calcite/controller/PlanController.java
@@ -2,15 +2,20 @@ package hk.hku.cs.calcite.controller;
 
 import hk.hku.cs.calcite.entity.Plan;
 import hk.hku.cs.calcite.service.PlanService;
+import org.apache.calcite.linq4j.function.Parameter;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
+//@Api(tags = "PlanController")
 public class PlanController {
     private static final Logger logger = LoggerFactory.getLogger(PlanController.class);
     private final AtomicInteger counter = new AtomicInteger();
@@ -21,8 +26,16 @@ public class PlanController {
         this.planService = planService;
     }
 
-    @GetMapping("/plan")
-    public Plan query(@RequestParam(value = "sql", defaultValue = "") String sql) {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello World";
+    }
+
+    @RequestMapping(value = "/plan", method = RequestMethod.POST)
+    @ResponseBody
+    public Plan query(@RequestBody Map<String, String> data) {
+        //@ApiParam("sql string") String sql
+        String sql = data.get("sql");
         logger.info("Input:\n" + sql);
 
         Plan plan = new Plan(counter.incrementAndGet(), sql);
@@ -33,6 +46,10 @@ public class PlanController {
         }
 
         logger.info("Output:\n" + plan.getPhysicalPlan());
+        SimpleDateFormat sdf = new SimpleDateFormat();// 格式化时间
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");// a为am/pm的标记
+        Date date = new Date();// 获取当前时间
+        logger.info(sdf.format(date));
         return plan;
     }
 }
